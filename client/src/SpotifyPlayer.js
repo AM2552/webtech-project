1f7f7e7f1e919063af8a9a9b9250a30fc5db7e05
Xandi <alexander.mosor@gmail.com>
2023-06-26 20:30:20 +0200

    Delete unused attribute

--- client/src/SpotifyPlayer.js
+++ client/src/SpotifyPlayer.js
@@ -37,7 +37,6 @@ const SpotifyPlayer = () => {
         src={`https://open.spotify.com/embed/playlist/${playlistId}?utm_source=generator&theme=0`}
         width="100%"
         height="152"
-        frameBorder="0"
         allowFullScreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
